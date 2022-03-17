281bae54074ed2b11e9e81e832c0704af107e783
Jonatan Antoni <jonatan.antoni@arm.com>
2022-03-17 15:50:50 +0100

    Add package/program version

--- arm/avhclient/__init__.py
+++ arm/avhclient/__init__.py
@@ -5,9 +5,13 @@
 # SPDX-License-Identifier: Apache-2.0
 #
 
+from importlib import metadata
+
 from .avh_client import AvhClient
 from .avh_backend import AvhBackend
 from .aws_backend import AwsBackend
 from .local_backend import LocalBackend
 
+__author__ = metadata.metadata('arm-avhclient')['Author']
+__version__ = metadata.version('arm-avhclient')
 __all__ = ['AvhClient', 'AvhBackend', 'AwsBackend', 'LocalBackend']
